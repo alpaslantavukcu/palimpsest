ebdca17276bea0f2d6ae38e67518d3416c5fc41b
Alpaslan Tavukçu <alpaslan.tavukcu@gmail.com>
2021-05-19 02:45:28 +0300

    Update sim_arch.py
    Initial gnss class implementation

--- sim/sim_arch.py
+++ sim/sim_arch.py
@@ -124,6 +124,51 @@ class JoystickControl:
         return self.parse_vehicle_wheel(controller)
 
 
+class GnssHelper:
+    def spawn(self, sim_world, blueprint, transform, attach):
+        self.blueprint = blueprint
+        self.transform = transform
+
+        with sim_world:
+            self.gnss = sim_world.spawn(self, attach)
+            self.gnss.listen(self.on_received_gnss_data)
+        
+        self.snapshot = sim_world.snapshot.find(self.gnss.id)
+    
+    def on_received_gnss_data(self, gnss_data):
+        x, y, alt = self.from_gps(gnss_data.latitude, gnss_data.longitude, gnss_data.altitude)
+        print("{}, {}".format(x, y))
+    
+    def from_gps(self, latitude: float, longitude: float, altitude: float):
+        """Creates Location from GPS (latitude, longitude, altitude).
+        This is the inverse of the _location_to_gps method found in
+        https://github.com/carla-simulator/scenario_runner/blob/master/srunner/tools/route_manipulation.py
+
+        https://github.com/erdos-project/pylot/blob/342c32eb598858ff23d7f24edec11414a3227885/pylot/utils.py
+        """
+        EARTH_RADIUS_EQUA = 6378137.0
+        # The following reference values are applicable for towns 1 through 7,
+        # and are taken from the corresponding OpenDrive map files.
+        # LAT_REF = 49.0
+        # LON_REF = 8.0
+        # TODO: Do not hardcode. Get the references from the open drive file.
+        LAT_REF = 49.0
+        LON_REF = 8.0
+
+        scale = math.cos(LAT_REF * math.pi / 180.0)
+        basex = scale * math.pi * EARTH_RADIUS_EQUA / 180.0 * LON_REF
+        basey = scale * EARTH_RADIUS_EQUA * math.log(
+            math.tan((90.0 + LAT_REF) * math.pi / 360.0))
+
+        x = scale * math.pi * EARTH_RADIUS_EQUA / 180.0 * longitude - basex
+        y = scale * EARTH_RADIUS_EQUA * math.log(
+            math.tan((90.0 + latitude) * math.pi / 360.0)) - basey
+
+        # This wasn't in the original method, but seems to be necessary.
+        y *= -1
+
+        return x, y, altitude
+
 # Kamera aktörünü temsil eden sınıf
 # Kamera fps artırmak için ne yapılabilir? Gerçek zamanlı kamera uygulaması için ne gerekli.
 
@@ -233,6 +278,7 @@ class Simulator:
         # Vehicle Init
         self.ego_vehicle = EgoVehicle()
         self.rgb_cam = Camera()
+        self.gnss = GnssHelper()
 
     def setup(self):
         # Vehicle Spawn
@@ -248,6 +294,10 @@ class Simulator:
         self.rgb_cam.spawn(self.sim_world, camera_bp, camera_transform, attach = self.ego_vehicle.car)
         print(self.rgb_cam.snapshot.get_transform())
 
+        # Gnss Spawn
+        gnss_bp = self.sim_world.blueprint_library.find('sensor.other.gnss')
+        self.gnss.spawn(self.sim_world, gnss_bp, carla.Transform(), attach=self.ego_vehicle.car)
+
         
         with self.sim_world:
             self.sim_world.spectator.set_transform(self.rgb_cam.snapshot.get_transform())
@@ -258,13 +308,13 @@ class Simulator:
         kb_control = KeyboardControl(self.ego_vehicle)
         kb_control.listen()
         """
-        js_control = JoystickControl()
+        #js_control = JoystickControl()
         try : 
             while True:
                 with self.sim_world:
                     self.sim_world.spectator.set_transform(self.sim_world.snapshot.find(self.rgb_cam.cam.id).get_transform())
-                    with self.ego_vehicle:
-                        js_control.get_control(self.ego_vehicle.controller)
+                    #with self.ego_vehicle:
+                    #    js_control.get_control(self.ego_vehicle.controller)
         except RuntimeError:
             pass
         finally:
