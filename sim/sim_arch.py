f0a87ddba9ae5301b5ed688e8626ab6f8d16be08
Alpaslan Tavukçu <alpaslan.tavukcu@gmail.com>
2021-05-25 21:48:12 +0300

    Line color (lane detector)

--- sim/sim_arch.py
+++ sim/sim_arch.py
@@ -412,8 +412,19 @@ class LaneDetectorHelper:
         cpy = img.copy()
         left_poly, right_poly, left, right = self.detector(img)
         lines = left + right
-        cpy[lines >= 0.5] = 255
         
+        ## Find better way !
+        cpy_r = cpy[:, :, 0]
+        cpy_g = cpy[:, :, 1]
+        cpy_b = cpy[:, :, 2]
+        cpy_r[lines >= 0.5] = 255
+        cpy_g[lines >= 0.5] = 0
+        cpy_b[lines >= 0.5] = 0
+
+        cpy[:, :, 0] = cpy_r
+        cpy[:, :, 1] = cpy_g
+        cpy[:, :, 2] = cpy_b
+
         return cpy
         #print(lines)
 
