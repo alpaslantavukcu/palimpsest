664b7c761e0fbd891658563f7045274a9c432543
Alpaslan Tavukçu <alpaslan.tavukcu@gmail.com>
2021-05-30 15:16:24 +0300

    Module seperation
    Simülasyon alt dosyalara ve modüllere ayrıştırıldı.

---
+++ sim/lane_detection/lane_detector_helper.py
@@ -0,0 +1,24 @@
+
+class LaneDetectorHelper:
+    def __init__(self):
+        self.detector = LaneDetector()
+    
+    def detect(self, img):
+        cpy = img.copy()
+        left_poly, right_poly, left, right = self.detector(img)
+        lines = left + right
+        
+        ## Find better way !
+        cpy_r = cpy[:, :, 0]
+        cpy_g = cpy[:, :, 1]
+        cpy_b = cpy[:, :, 2]
+        cpy_r[lines >= 0.5] = 255
+        cpy_g[lines >= 0.5] = 0
+        cpy_b[lines >= 0.5] = 0
+
+        cpy[:, :, 0] = cpy_r
+        cpy[:, :, 1] = cpy_g
+        cpy[:, :, 2] = cpy_b
+
+        return cpy
+        #print(lines)
