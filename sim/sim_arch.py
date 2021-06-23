579fc3b027dab92181fbcee0cd42b7d1fbd3c2be
Alpaslan Tavukçu <alpaslan.tavukcu@gmail.com>
2021-06-23 04:00:02 +0300

    Update sim_arch.py

--- sim/sim_arch.py
+++ sim/sim_arch.py
@@ -126,8 +126,8 @@ class Simulator:
     def setup(self):
         # Vehicle Spawn
         ego_blueprint = self.sim_world.blueprint_library.find('vehicle.mini.cooperst')
-        #ego_transform = carla.Transform(carla.Location(x=-88.3, y=21.5, z=0.35), carla.Rotation(pitch=0.35, yaw=89.8, roll=-0.0))
-        ego_transform = carla.Transform(carla.Location(x=-88.6, y=151.9, z=0.35), carla.Rotation(pitch=0.35, yaw=89.8, roll=-0.0))
+        ego_transform = carla.Transform(carla.Location(x=-88.3, y=21.5, z=0.35), carla.Rotation(pitch=0.35, yaw=89.8, roll=-0.0))
+        #ego_transform = carla.Transform(carla.Location(x=-88.6, y=151.9, z=0.35), carla.Rotation(pitch=0.35, yaw=89.8, roll=-0.0))
         self.ego_vehicle.spawn(self.sim_world, ego_blueprint, ego_transform)
 
         # Camera Spawns
@@ -329,20 +329,17 @@ class Simulator:
                                 speed = np.linalg.norm( carla_vec_to_np_array(self.ego_vehicle.car.get_velocity()))
 
                                 traj = self.ldetector.get_trajectory_from_lane_detector()
-                                throttle, steer = self.controller.get_control(traj, speed, desired_speed=10, dt=0.1)        
-                                self.ego_vehicle.controller.steer = steer
-                                self.ego_vehicle.controller.throttle = 0.4
+                                
 
-                                #js_control.get_control(self.ego_vehicle.controller)
-                                """
+                                
                                 if kb_control.flag:
                                     traj = self.ldetector.get_trajectory_from_lane_detector()
                                     throttle, steer = self.controller.get_control(traj, speed, desired_speed=10, dt=0.1)
                                     self.ego_vehicle.controller.steer = steer
+                                    self.ego_vehicle.controller.throttle = 0.4
                                     #self.ego_vehicle.controller.throttle = np.clip(throttle, 0, 0.7)
                                 else:
                                     js_control.get_control(self.ego_vehicle.controller)
-                                """
 
                             Vx = self.ego_vehicle.car.get_velocity().x
                             Yr = self.imu.Yr
